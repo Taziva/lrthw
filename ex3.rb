6edc7e797885fe9901626ca3d3fe96cb7b88799d
Shoniwa <stmarovatsanga@gmail.com>
2016-11-30 18:29:46 +0000

    First 5 exercises

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+# Prints string
+puts "I will now count my chickens:"
+# Prints String and the answer to the inserted sum
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Prints String and the answer to the inserted sum
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# Prints String
+puts "Now I will count the eggs:"
+# Prints answer to the sum
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# Prints String
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Prints boolean of either true or false
+puts 3.0 + 2.0 < 5.0 - 7.0
+# Prints String and the answer to the inserted sum
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# Prints String and the answer to the inserted sum
+puts "What is 5 - 7? #{5.0 - 7.0}"
+# Prints String
+puts "Oh, that's why it's false."
+# Prints String
+puts "How about some more."
+# Prints string and boolean of either true or false
+puts "Is it greater? #{5.0 > -2.0}"
+# Prints string and boolean of either true or false
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Prints string and boolean of either true or false
+puts "Is it less or equal? #{5.0 <= -2.0}"
