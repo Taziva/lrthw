69c7698024f2a5f474564f3aff59957115f5b932
Shoniwa <stmarovatsanga@gmail.com>
2016-12-01 15:19:59 +0000

    Exercise 6-10

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+#Assigns 10 to a variable
+types_of_people = 10
+#Assign a string to a variable
+x = "There are #{types_of_people} types of people."
+#Assign a string to a variable
+binary = "binary"
+#Assign a string to a variable
+do_not = "don't"
+#Assign a string to a variable and insert 2 variables into the string
+y = "Those who know #{binary} and those who #{do_not}."
+#Prints the string assigned to the variable
+puts x
+#Prints the string assigned to the variable
+puts y
+#Prints the string and inserts a variable into the string
+puts "I said: #{x}."
+#Prints the string and inserts a variable into the string
+puts "I also said: '#{y}'."
+#Assigns a boolean to a variable
+hilarious = false
+#Assign a string to a variable and insert a variable into the string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#Prints the string assigned to the variable
+puts joke_evaluation
+#Assign a string to a variable
+w = "This is the left side of..."
+#Assign a string to a variable
+e = "a string with a right side."
+#Prints a concatenated string comprised of the strings assigned to the 2 variables
+puts w + e
---
+++ ex7.rb
@@ -0,0 +1,38 @@
+# Prints string
+puts "Mary had a little lamb."
+# Prints string and inserts string into string
+puts "Its fleece was as #{'snow'}."
+# Prints string
+puts "And everywhere that Mary went."
+# Prints string 10 times
+puts "." * 10 #what'd that do?
+# Assigns letter to variable
+end1 = "C"
+# Assigns letter to variable
+end2 = "h"
+# Assigns letter to variable
+end3 = "e"
+# Assigns letter to variable
+end4 = "e"
+# Assigns letter to variable
+end5 = "s"
+# Assigns letter to variable
+end6 = "e"
+# Assigns letter to variable
+end7 = "B"
+# Assigns letter to variable
+end8 = "u"
+# Assigns letter to variable
+end9 = "r"
+# Assigns letter to variable
+end10 = "g"
+# Assigns letter to variable
+end11 = "e"
+# Assigns letter to variable
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Print string from a concatenation of several variables
+print end1 + end2 + end3 + end4 + end5 + end6
+# Print string from a concatenation of several variables
+puts end7 + end8 + end9 + end10 + end11 + end12
